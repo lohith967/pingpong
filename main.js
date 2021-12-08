756d35ba9452e79d426b113794c35c4a4e38bbc9
lohith967 <72453060+lohith967@users.noreply.github.com>
2021-12-08 19:53:03 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,11 +1,19 @@
+rightWristX="";
+rightWristY="";
+scorerightWrist = 0;
+
 function preload(){
 }
 
+function start(){
+  Gamestatus="start Game";
+  document.getElementById("status").innerHTML = "Game is loading!!"
+  }
+
 function setup(){
-    canvas =  createCanvas(600,500);
-  canvas.center();
-  video=createCapture(VIDEO);
-  video.size(400, 400);
+  video= createCapture(VIDEO);
+	video.size(800,400);
+	video.parent("Game_console");
   
   poseNet=ml5.poseNet(video,modelLoaded);
   poseNet.on('pose',gotPoses);
@@ -16,3 +24,25 @@ function modelLoaded(){
     console.log("Model Loaded");
 }
 
+function draw(){
+  if(scorerightWrist >0.2){
+  fill("red");
+  stroke("red");
+  circle(rightWristX,rightWristY,20.5);
+  }
+}
+
+function gotPoses(results){
+	if(results.length >0){
+
+		console.log(results);
+    scorelrightWrist = results[0].pose.keypoints[10].score;
+    rightWristX = results[0].pose.rightWrist.x;
+    rightWristY = results[0].pose.rightWrist.y;
+
+    console.log("rightWristX = "+rightWristX+"rightWristY = "+rightWristY);
+
+		
+
+	}
+}
